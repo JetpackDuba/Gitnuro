25990253fa2d9f95950d7cf04fd50c897356aecd
Abdelilah El Aissaoui <aeab13@gmail.com>
2021-10-06 19:07:41 +0200

    Fixed broken import

--- src/main/kotlin/app/git/GitManager.kt
+++ src/main/kotlin/app/git/GitManager.kt
@@ -11,7 +11,7 @@ import org.eclipse.jgit.lib.Ref
 import org.eclipse.jgit.lib.Repository
 import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import GPreferences
+import app.GPreferences
 import java.io.File
 
 
