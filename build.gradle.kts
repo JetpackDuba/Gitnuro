a948e2da066bcdef8c55b1d33d82eb342289fe61
Abdelilah El Aissaoui <aeab13@gmail.com>
2021-12-01 18:54:21 +0100

    Updated compose to 1.0.0-rc12

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     kotlin("kapt") version "1.5.31"
     kotlin("plugin.serialization") version "1.5.31"
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    id("org.jetbrains.compose") version "1.0.0-rc5"
+    id("org.jetbrains.compose") version "1.0.0-rc12"
 }
 
 group = "aeab13.github"
