685769a154019445381511ae2aec48236a636ec2
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-04-07 18:44:52 +0200

    Increased project version to 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 // Remember to update Constants.APP_VERSION when changing this version
-val projectVersion = "0.1.0"
+val projectVersion = "0.1.1"
 val projectName = "Gitnuro"
 
 group = "com.jetpackduba"
