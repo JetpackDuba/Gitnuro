69bbfff3574de67347836099423420b7eab9fe20
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-05-03 19:49:53 +0200

    Updated gradle version to prepare the next version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 // Remember to update Constants.APP_VERSION when changing this version
-val projectVersion = "0.1.1"
+val projectVersion = "0.2.0"
 val projectName = "Gitnuro"
 
 group = "com.jetpackduba"
