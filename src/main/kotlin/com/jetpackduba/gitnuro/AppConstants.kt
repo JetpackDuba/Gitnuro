b4391880aca9a9175a10b950d903898fbc1b9fe9
Abdelilah El Aissaoui <aeab13@gmail.com>
2023-05-02 19:33:17 +0200

    Increased version code to prepare new release

--- src/main/kotlin/com/jetpackduba/gitnuro/AppConstants.kt
+++ src/main/kotlin/com/jetpackduba/gitnuro/AppConstants.kt
@@ -23,7 +23,7 @@ object AppConstants {
     const val APP_DESCRIPTION =
         "Gitnuro is a Git client that allows you to manage multiple repositories with a modern experience and live visual representation of your repositories' state."
     const val APP_VERSION = "1.2.0"
-    const val APP_VERSION_CODE = 7
+    const val APP_VERSION_CODE = 8
     const val VERSION_CHECK_URL = "https://raw.githubusercontent.com/JetpackDuba/Gitnuro/main/latest.json"
 }
 
