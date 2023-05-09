905f7805bf493dc8fdc1a107205a31f24ee06a3c
Abdelilah El Aissaoui <aeab13@gmail.com>
2023-05-09 18:30:20 +0200

    Enabled support by default for all supported formats by libssh
    Fixes #108

--- src/main/kotlin/com/jetpackduba/gitnuro/credentials/GRemoteSession.kt
+++ src/main/kotlin/com/jetpackduba/gitnuro/credentials/GRemoteSession.kt
@@ -38,6 +38,7 @@ class GRemoteSession @Inject constructor(
         val session = processSession.get()
         session.setOptions(LibSshOptions.SSH_OPTIONS_HOST, uri.host)
         session.setOptions(LibSshOptions.SSH_OPTIONS_USER, uri.user)
+        session.setOptions(LibSshOptions.SSH_OPTIONS_PUBLICKEY_ACCEPTED_TYPES, "ssh-ed25519,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521,ssh-rsa,rsa-sha2-512,rsa-sha2-256,ssh-dss")
         session.loadOptionsFromConfig()
 
         session.connect()
