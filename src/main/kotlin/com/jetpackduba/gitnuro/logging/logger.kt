e541c60fc2efba41cb9976c7fba9b9f776ab2fb2
Abdelilah El Aissaoui <aeab13@gmail.com>
2023-07-26 19:12:19 +0200

    Fixed breaking changes from kotlin-logging

--- src/main/kotlin/com/jetpackduba/gitnuro/logging/logger.kt
+++ src/main/kotlin/com/jetpackduba/gitnuro/logging/logger.kt
@@ -1,17 +1,17 @@
 package com.jetpackduba.gitnuro.logging
 
-import io.github.oshai.KotlinLogging
+import io.github.oshai.kotlinlogging.KotlinLogging
 
 val logger = KotlinLogging.logger("org.slf4j")
 
 fun printLog(tag: String, message: String) {
-    logger.info("$tag - $message")
+    logger.info { "$tag - $message" }
 }
 
 fun printDebug(tag: String, message: String) {
-    logger.debug("$tag - $message")
+    logger.debug { "$tag - $message" }
 }
 
 fun printError(tag: String, message: String, e: Exception? = null) {
-    logger.error("$tag - $message", e)
+    logger.error(e) { "$tag - $message" }
 }
