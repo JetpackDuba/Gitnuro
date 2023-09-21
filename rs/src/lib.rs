aa806387421e6cd0781ba090826d847487b568d2
Abdelilah El Aissaoui <aeab13@gmail.com>
2023-09-21 21:19:47 +0200

    Disabled warning on unused import warning because the import is needed to be mapped to an enum in the UDL file

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -3,9 +3,7 @@ mod watch_directory;
 
 use watch_directory::{ * };
 use ssh::{ * };
-#[allow(unused_imports)]
+#[allow(unused_imports)] // Needed to map it to the enum in the UDL file
 use libssh_rs::AuthStatus;
 
 uniffi::include_scaffolding!("gitnuro");
-
-
