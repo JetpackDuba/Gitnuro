9fea38f203c1060fa08bce13173940731a33729e
Abdelilah El Aissaoui <aeab13@gmail.com>
2024-08-18 20:47:31 +0200

    Moved to latest compose dev build

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
     id("com.google.devtools.ksp") version "2.0.0-1.0.21"
-    id("org.jetbrains.compose") version "1.7.0-dev1750"
+    id("org.jetbrains.compose") version "1.7.0-dev1756"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
