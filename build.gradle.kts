dfa9ea2307eee7aeed685d869c992b945904133e
Abdelilah El Aissaoui <aeab13@gmail.com>
2024-09-02 15:52:29 +0200

    Moved to Kotlin 2.0.20 and newer compose dev build

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ val linuxX64Target = "x86_64-unknown-linux-gnu"
 plugins {
     // Kotlin version must match compose version
     kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0"
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21"
-    id("org.jetbrains.compose") version "1.7.0-dev1756"
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    kotlin("plugin.serialization") version "2.0.20"
+    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
+    id("org.jetbrains.compose") version "1.7.0-alpha03"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
 }
 
 // Remember to update Constants.APP_VERSION when changing this version
