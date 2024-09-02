ea4572b91b7cab7b02b290c2125743d49f964097
Abdelilah El Aissaoui <aeab13@gmail.com>
2024-09-02 15:58:43 +0200

    Fixed delete tag calling wrong use case

--- src/main/kotlin/com/jetpackduba/gitnuro/viewmodels/SharedTagsViewModel.kt
+++ src/main/kotlin/com/jetpackduba/gitnuro/viewmodels/SharedTagsViewModel.kt
@@ -5,6 +5,7 @@ import com.jetpackduba.gitnuro.extensions.simpleName
 import com.jetpackduba.gitnuro.git.RefreshType
 import com.jetpackduba.gitnuro.git.TabState
 import com.jetpackduba.gitnuro.git.branches.CheckoutRefUseCase
+import com.jetpackduba.gitnuro.git.tags.DeleteTagUseCase
 import com.jetpackduba.gitnuro.models.positiveNotification
 import kotlinx.coroutines.Job
 import org.eclipse.jgit.lib.Ref
@@ -15,7 +16,7 @@ interface ISharedTagsViewModel {
 }
 
 class SharedTagsViewModel @Inject constructor(
-    private val deleteTagUseCase: CheckoutRefUseCase,
+    private val deleteTagUseCase: DeleteTagUseCase,
     private val tabState: TabState,
 ) : ISharedTagsViewModel {
     override fun deleteTag(tag: Ref) = tabState.safeProcessing(
