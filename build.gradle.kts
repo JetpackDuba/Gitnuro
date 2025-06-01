3c5194caa493eddcf46ef389c351cf0f98018794
Abdelilah El Aissaoui <aeab13@gmail.com>
2025-06-01 21:32:33 +0200

    Replaced custom image load with Coil

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,6 +84,9 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.client.logging)
+
+    implementation(libs.coil3.compose)
+    implementation(libs.coil3.network.okhttp)
 }
 
 fun currentOs(): OS {
