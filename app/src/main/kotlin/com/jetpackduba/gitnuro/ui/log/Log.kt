73e97165bdc186bb3257daf326fd8773f091cf0b
Abdelilah El Aissaoui <aeab13@gmail.com>
2026-09-18 11:03:47 +0200

    Added keys to log commits to improve performance

--- app/src/main/kotlin/com/jetpackduba/gitnuro/ui/log/Log.kt
+++ app/src/main/kotlin/com/jetpackduba/gitnuro/ui/log/Log.kt
@@ -550,9 +550,9 @@ fun CommitsList(
         // TODO This should be improved in case it's a dangling branch, shouldn't happen often but could be a thing
         items(
             items = commitList.values.toList(),
-            /*key = { commit ->
-                commit.hash + branches[commit.hash].orEmpty().joinToString() + tags[commit.hash].orEmpty().joinToString()
-            },*/
+            key = { commit ->
+                commit.hash + commit.lane + branches[commit.hash].orEmpty().joinToString() + tags[commit.hash].orEmpty().joinToString()
+            },
         )
         { graphNode ->
             CommitLine(
